6dfad4828bec49a289091ab73d42d456bb102b08
Pedro Marques <116228124+phxdablio@users.noreply.github.com>
2023-12-08 00:26:57 -0300

    Add files via upload

---
+++ ex4/Animal.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ex44;
+
+/**
+ *
+ * @author Dell Pentium Gold
+ */
+public class Animal {
+    
+}
